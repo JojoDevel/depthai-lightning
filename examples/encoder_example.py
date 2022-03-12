b46bc29a81f251cab2a1281697cb9f6a9c833df1
JojoDevel <14841215+JojoDevel@users.noreply.github.com>
2022-03-12 21:17:36 +0100

    Add video encoding example

---
+++ examples/encoder_example.py
@@ -0,0 +1,31 @@
+"""Simple example for camera usage
+"""
+
+import cv2
+
+from depthai_lightning import PipelineManager
+from depthai_lightning.nodes import ColorCamera, EncodingConfig, LiveView, VideoEncoder
+
+if __name__ == "__main__":
+
+    pm = PipelineManager()
+
+    # create high-level nodes
+
+    # color camera
+    cam = ColorCamera(pm)
+    # live view for video
+    liveView = LiveView(pm, cam, "video")  # or raw, preview
+    # encoder for video
+    enc = VideoEncoder(pm, "record", EncodingConfig.LOW, cam, True)
+
+    # create the pipeline
+    with pm:
+        while True:
+            # show video stream
+            liveView.show()
+            # encode video stream
+            enc.write()
+
+            if cv2.waitKey(1) == ord("q"):
+                break
